c2c966d56fb8ab8327c0a6dd54e304d6b0e4e384
Ekaterina <129519674+beeonlune@users.noreply.github.com>
2023-04-01 02:27:49 +0300

    Add files via upload

---
+++ mysite/mysite/script.js
@@ -0,0 +1,4 @@
+$("button").click(function(){
+    $("figure img").attr("src", "fall.gif");
+    $("figure:first p").text("NONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONONO");
+})
